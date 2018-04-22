4a142326d0f827b754ece26a7c109eef72b2dedf
maokailu <1113901189@qq.com>
2018-04-22 23:59:24 +0800

    start list page

--- src/air-booking/list.jsx
+++ src/air-booking/list.jsx
@@ -5,37 +5,37 @@ export default class List extends React.Component {
     constructor() {
         super();
     }
-    
+
     state = {
-      flights: null,
-      test: null
-  }
-    componentDidMount() {
-      utils.getPromise('http://localhost:8080/fecthFlights').then(json => {
-        console.log('Contents: ' + json);
-        this.setState({
-            flights: json,
-            test: json
-        });
-    }, error => {
-        console.error('出错了', error);
-    });
+        flights: null,
+        test: null
     }
-  render() {
-      return (
-          <div className="list">
-            {this.state.flights && this.state.flights.map((flight, index) =>
-                <div key={index} className="item">
-                    <div className="row1">
-                        <img className="logo" src="http://pic.english.c-ctrip.com/airline_logo/32/zh.png"/>
-                        <span className="airline">{flight.name}</span>
-                    </div>
-                    <div className="row2">
+    componentDidMount() {
+        utils.getPromise('http://localhost:8080/fecthFlights').then(json => {
+            console.log('Contents: ' + json);
+            this.setState({
+                flights: json,
+                test: json
+            });
+        }, error => {
+            console.error('出错了', error);
+        });
+    }
+    render() {
+        return (
+            <div className="list">
+                {this.state.flights && this.state.flights.map((flight, index) =>
+                    <div key={index} className="item">
+                        <div className="row1">
+                            <img className="logo" src="http://pic.english.c-ctrip.com/airline_logo/32/zh.png"/>
+                            <span className="airline">{flight.name}</span>
+                        </div>
+                        <div className="row2">
                         14:40
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
-      );
-  }
+                )}
+            </div>
+        );
+    }
 }
