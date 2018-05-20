f998645a79b0ad3bc7d1ac159a42ad96ef2adc14
maokl <maokl@Ctrip.com>
2018-05-21 06:33:35 +0800

    完成帮助页面

--- src/controller/header/help/index.jsx
+++ src/controller/header/help/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.scss';
+import Header from 'header';
 export default class Help extends React.Component {
     constructor(props) {
         super(props);
@@ -9,38 +10,41 @@ export default class Help extends React.Component {
     }
     render() {
         return (
-            <div className="help">
-                <div className="title">
-                    FAQ
-                </div>
-                <div className="us">
-                    <div className="left">
-                        <div className="country">United States</div>
-                        <div className="phone">833 896 0077</div>
-                        <div className="info">24/7</div>
+            <div>
+                <Header />
+                <div className="help">
+                    <div className="title">
+                        常见问题
                     </div>
-                    <div className="right">
-                        电话
+                    <div className="us">
+                        <div className="left">
+                            <div className="country">美国</div>
+                            <div className="phone">833 896 0077</div>
+                            <div className="help-info">24/7</div>
+                        </div>
+                        <div className="right">
+                            电话
+                        </div>
                     </div>
-                </div>
-                <div className="cn">
-                    <div className="left">
-                        <div className="country">China</div>
-                        <div className="phone">400 828 8966</div>
-                        <div className="info">24/7</div>
+                    <div className="cn">
+                        <div className="left">
+                            <div className="country">中国</div>
+                            <div className="phone">400 828 8966</div>
+                            <div className="help-info">24/7</div>
+                        </div>
+                        <div className="right">
+                            电话
+                        </div>
                     </div>
-                    <div className="right">
-                        电话
-                    </div>
-                </div>
-                <div className="others">
-                    <div className="left">
-                        <div className="country">Other Locations</div>
-                        <div className="phone">+86 21 3210 4669</div>
-                        <div className="info">24/7</div>
-                    </div>
-                    <div className="right">
-                        电话
+                    <div className="others">
+                        <div className="left">
+                            <div className="country">其他地区</div>
+                            <div className="phone">+86 21 3210 4669</div>
+                            <div className="help-info">24/7</div>
+                        </div>
+                        <div className="right">
+                            电话
+                        </div>
                     </div>
                 </div>
             </div>
